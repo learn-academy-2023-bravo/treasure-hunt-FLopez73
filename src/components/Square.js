d85cfe5a9e1e73d588b22afa97ecf656d253c799
FLopez73 <flopez730@gmail.com>
2023-04-03 12:24:58 -0700

    left off at count tracker

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,9 +1,15 @@
 import React from "react"
 
-const Square = () => {
+const Square = ({ square, index, handleGamePlay }) => {
+  const handelClick = () => {
+    handleGamePlay(index)
+  }
+
   return (
     <>
-      <div className="square"></div>
+      <div className="square" onClick={handelClick}>
+        {square}
+      </div>
     </>
   )
 }
